3cc82d7a24ac6a18e2ae3d8f2c8e42ba4df6ae4e
cwavedave <david@creativewavelength.co.uk>
2021-01-20 12:52:35 +0100

    Countries file added / fixed for formatting

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
+import pandas as pd
+
+countries_csv = pd.read_csv("countries.csv")
+countries_df = pd.DataFrame(countries_csv)
+
+#Strips white Space from all columns: https://stackoverflow.com/questions/33788913/pythonic-efficient-way-to-strip-whitespace-from-every-pandas-data-frame-cell-tha
+df = countries_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+print(df)
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
 latitude = data ["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
 
 iss_position = (longitude,latitude)
 
