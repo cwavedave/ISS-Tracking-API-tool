561e521ed59b1f5c3135eb18bbc37e26c90e8100
cwavedave <david@creativewavelength.co.uk>
2021-01-22 18:14:55 +0100

    Fixed STR issue in database & corrupted data from incorrect formatting

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import pandas as pd
 import datetime as dt
 
 now = dt.datetime.now()
-
-print(now)
-
 countries_csv = pd.read_csv("countries.csv", sep='\s*,\s*', engine='python')
-countries_df = pd.DataFrame(countries_csv)
+df = pd.DataFrame(countries_csv)
+df['latitude'] = df['latitude'].astype(float)
 
-# #Strips white Space from all columns: https://stackoverflow.com/questions/33788913/pythonic-efficient-way-to-strip-whitespace-from-every-pandas-data-frame-cell-tha
-# df = countries_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-# print(df)
+
+# print(df['latitude'])
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -21,14 +18,53 @@ data = response.json()
 latitude = float(data["iss_position"]["latitude"])
 longitude = float(data["iss_position"]["longitude"])
 
-# print(countries_df['longitude'])
+print(df.iloc[1]['latitude'])
+
+latitudes = df['latitude'].to_list()
+print('Latitude')
+print(latitudes)
+
+longitudes = df['longitude'].to_list()
+print('Longitude')
+print(longitudes)
+
+for l in latitudes:
+    if isinstance(l,str):
+        print("String Located")
+
+# def check_lat():
+#     nearby_lat = []
+#     for l in latitudes:
+#         if l > latitude -5 and l < latitude + 5:
+#             nearby_lat.append(l)
+#     return nearby_lat
+#
+# def check_long():
+#     nearby_long = []
+#     for l in longitudes:
+#         if l > longitude -5 and l < longitude + 5:
+#             nearby_long.append(l)
+#     return nearby_long
+
+# print(check_long())
+# print(check_lat())
+
+# latitude_match = df[df['latitude'].between(0, 100)]
+# latitude_match_2 = df[df['latitude'].between(0,100)]
+
+# print(latitude_match)
+# print(latitude_match_2)
+
+
 # Attempt 1 - To find closest
 # index = abs(countries_df['latitude'] - (latitude).idxmin())
 # print(index)
-#
+
+
+
 iss_position = (longitude,latitude)
 
-print(iss_position)
+# print(iss_position)
 
 vic_lo = 2.25486
 vic_la = 41.93012
@@ -37,10 +73,10 @@ vic_la = 41.93012
 response = requests.get(url=f'https://api.sunrise-sunset.org/json?lat={vic_la}&lng={vic_lo}&formatted=0')
 sun_times = response.json()
 
-print(sun_times)
+# print(sun_times)
 
 current_time = int(str(now).split(' ')[1].split('.')[0][:-3].replace(":",""))
-print(current_time)
+# print(current_time)
 
 sunrise = int(sun_times["results"]["sunrise"].split("T")[1].split("+")[0][:-3].replace(":",""))
 sunset = int(sun_times["results"]["sunset"].split("T")[1].split("+")[0][:-3].replace(":",""))
@@ -58,4 +94,4 @@ else:
 
 print(f"Sunset time is {str(sunset)[:-2] + ':' + list(str(sunset))[2] + list(str(sunset))[3]}")
 
-print(sunset)
+# print(sunset)
