adb36c78906e67ef4c2008912889870e6bb51d96
cwavedave <david@creativewavelength.co.uk>
2021-01-23 18:33:20 +0100

    Refactoring

--- main.py
+++ main.py
@@ -7,44 +7,32 @@ countries_csv = pd.read_csv("countries.csv", sep='\s*,\s*', engine='python')
 df = pd.DataFrame(countries_csv)
 df['latitude'] = df['latitude'].astype(float)
 
+def get_iss_location():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    latitude = float(data["iss_position"]["latitude"])
+    longitude = float(data["iss_position"]["longitude"])
+    return (latitude,longitude)
 
-# print(df['latitude'])
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-
-data = response.json()
-
-latitude = float(data["iss_position"]["latitude"])
-longitude = float(data["iss_position"]["longitude"])
-
-print(f"ISS locations is {(latitude,longitude)}")
-
-print(df.iloc[1]['latitude'])
+iss_location = get_iss_location()
+print(f"ISS locations is {(iss_location)}")
 
 latitudes = df['latitude'].to_list()
-
 longitudes = df['longitude'].to_list()
 
 for l in latitudes:
     if isinstance(l,str):
         print("String Located")
 
-latitude_match = df[df['latitude'].between(latitude -5, latitude + 5)]
+nearby_countries = df[df['longitude'].between(iss_location[1] -15, iss_location[1] + 15) & df['latitude'].between(iss_location[0] -15, iss_location[0] + 15)]
 
-longitude_match = df[df['longitude'].between(longitude -5, longitude + 5)]
+# Current limited use is, must be near the center of the country.
+countries_nearby_list = nearby_countries['name'].to_list()
 
-nearby_countries = df[df['longitude'].between(longitude -5, longitude + 5) & df['latitude'].between(latitude -5, latitude + 5)]
-print("Closest Countries to the ISS")
-print(nearby_countries)
-
-# Attempt 1 - To find closest
-# index = abs(countries_df['latitude'] - (latitude).idxmin())
-# print(index)
-
-iss_position = (longitude,latitude)
-
-# print(iss_position)
+if len(countries_nearby_list) != 0:
+    print(countries_nearby_list)
+    print(f"Number of nearby countries - {len(countries_nearby_list)}")
 
 vic_lo = 2.25486
 vic_la = 41.93012
@@ -64,8 +52,19 @@ sunset = int(sun_times["results"]["sunset"].split("T")[1].split("+")[0][:-3].rep
 if sunset > current_time:
     print(f"Current time is {sunrise}")
     time_until_sunset = sunset - current_time
-    print(f"Calculate time until sunset in Vic - {str(time_until_sunset)[:-2] + ':' + list(str(time_until_sunset))[1] + list(str(time_until_sunset))[2]}")
+    print(time_until_sunset)
+    if len(str(time_until_sunset)) <= 2:
+        print(f"{time_until_sunset} Minutes left until sunset in Vic")
+    else:
+        print(f"Calculate time until sunset in Vic - {str(time_until_sunset)[:-2] + ':' + list(str(time_until_sunset))[1] + list(str(time_until_sunset))[2]}")
     print(f"Sunset time is {sunset}")
+
+if sunrise > current_time:
+    print("Time until Sunrise ( Morning ) ")
+
+if sunrise < current_time and sunset < current_time:
+    sunrise_time_tomorrow = 2400 - current_time + sunrise
+    print(f"Time until sunrise tomorrow {sunrise_time_tomorrow}")
 
 if len(str(sunrise)) == 3:
     print(f"Sunrise time is {str(sunrise)[:-2] + ':' + list(str(sunrise))[1] + list(str(sunrise))[2]}")
@@ -74,5 +73,3 @@ else:
     print(f"Sunrise time is {str(sunrise)[:-2] + ':' + list(str(sunrise))[2] + list(str(sunrise))[3]}")
 
 print(f"Sunset time is {str(sunset)[:-2] + ':' + list(str(sunset))[2] + list(str(sunset))[3]}")
-
-# print(sunset)
